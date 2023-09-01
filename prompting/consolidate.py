4980b7c8128d4c4715ec6fd803d9193c343cddd7
acq22jal <joaoaugustobr@hotmail.com>
2023-09-01 18:36:40 +0100

    fixing consolidate

--- prompting/consolidate.py
+++ prompting/consolidate.py
@@ -10,11 +10,16 @@ VERBOSE=False
 CACHE_FOLDER = f"data/caches/{DATASET}/{MODEL_NAME}/"
 CACHE_PATH = CACHE_FOLDER + "cache.jsonl"
 PROCESSED_FOLDER = f"data/processed/{DATASET}/{MODEL_NAME}/"
+DATASET_PATH = f"data/datasets/{DATASET}.csv"
 
 if not os.path.exists(PROCESSED_FOLDER):
     os.makedirs(PROCESSED_FOLDER)
 
+df_fn = pd.read_csv(DATASET_PATH)
+df_fn = df_fn[["article_md5", "title", "text"]]
+
 df = pd.read_json(CACHE_PATH, lines=True)
+df = df.merge(df_fn, on="article_md5")
 df = df.drop_duplicates(["article_md5"])
 df = df.reset_index(drop=True)
 df.to_csv(PROCESSED_FOLDER+f"{DATASET}.csv", index=False)
