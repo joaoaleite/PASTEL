fe92d92beb4f0924721fd400b9377ed1503e4972
Joao Leite <joaoaugustobr@hotmail.com>
2023-09-11 14:48:56 +0100

    training script done

--- training/model.py
+++ training/model.py
@@ -1,15 +1,21 @@
 import torch
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+import wandb
+from transformers import (
+    AdamW,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup,
+)
 
 
 class FakeDataset(Dataset):
-    def __init__(self, data, targets, max_len=512):
+    def __init__(self, data, targets, tokenizer_name, max_len=512):
         self.data = data
         self.targets = targets
-        self.tokenizer = self.tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+        self.tokenizer = self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
         self.max_len = max_len
     
     def __len__(self):
@@ -34,11 +40,17 @@ class FakeDataset(Dataset):
         }
 
 class TransformersClassifier(pl.LightningModule):
-    def __init__(self, num_classes, learning_rate=2e-5):
-        super(TransformersClassifier, self).__init__()
+    def __init__(
+            self,
+            pretrained_name,
+            learning_rate,
+            warmup_steps,
+            weight_decay,
+            num_classes=1):
+        super().__init__()
         self.save_hyperparameters()
-        self.model = AutoModelForSequenceClassification.from_pretrained('roberta-base', num_labels=num_classes)
-        self.learning_rate = learning_rate
+        self.model = AutoModelForSequenceClassification.from_pretrained(pretrained_name, num_labels=num_classes)
+        self.validation_step_outputs = []
 
     def forward(self, input_ids, attention_mask, targets=None):
         return self.model(input_ids=input_ids, attention_mask=attention_mask, labels=targets)
@@ -51,34 +63,78 @@ class TransformersClassifier(pl.LightningModule):
         outputs = self(input_ids, attention_mask, targets)
         loss = outputs.loss
 
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('end_classifier/train_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         input_ids, attention_mask, targets = batch.values()
-
         outputs = self(input_ids, attention_mask, targets)
         val_loss = outputs.loss
         logits = outputs.logits
-        preds = torch.argmax(logits, dim=1)
+        probs = torch.nn.Sigmoid()(logits)
+        preds = torch.zeros(probs.shape)
+        preds[torch.where(probs >= 0.5)] = 1
+        outputs = {"loss": val_loss, "preds": preds, "labels": targets}
 
-        val_acc = accuracy_score(targets.cpu(), preds.cpu())
-        val_f1_macro = f1_score(targets.cpu(), preds.cpu(), average='macro')
+        self.validation_step_outputs.append(outputs)
 
-        tn, fp, fn, tp = confusion_matrix(targets.cpu(), preds.cpu()).ravel()
+        return outputs
+
+    def on_validation_epoch_end(self):
+        if self.global_step == 0:
+            wandb.define_metric('end_classifier/val_f1_macro', summary='max')
+            wandb.define_metric('end_classifier/val_acc', summary='max')
+            wandb.define_metric('end_classifier/val_loss', summary='min')
+            wandb.define_metric('end_classifier/val_false_positive_rate', summary='min')
+            wandb.define_metric('end_classifier/val_true_negative_rate', summary='min')
+            wandb.define_metric('end_classifier/val_false_negative_rate', summary='min')
+            wandb.define_metric('end_classifier/val_true_positive_rate', summary='min')
+            
+    
+        preds = torch.cat([x["preds"] for x in self.validation_step_outputs]).detach().cpu().numpy()
+        targets = torch.cat([x["labels"] for x in self.validation_step_outputs]).detach().cpu().numpy()
+        val_loss = torch.stack([x["loss"] for x in self.validation_step_outputs]).mean()
+
+
+        val_acc = accuracy_score(targets, preds)
+        val_f1_macro = f1_score(targets, preds, average='macro', zero_division=0.0)
+
+        tn, fp, fn, tp = confusion_matrix(targets, preds).ravel()
         false_positive_rate = fp / (fp + tn)
         true_negative_rate = tn / (tn + fp)
         false_negative_rate = fn / (fn + tp)
         true_positive_rate = tp / (tp + fn)
 
-        self.log('val_loss', val_loss, prog_bar=True)
-        self.log('val_acc', val_acc, prog_bar=True)
-        self.log('val_f1_macro', val_f1_macro, prog_bar=True)
-        self.log('val_false_positive_rate', false_positive_rate, prog_bar=True)
-        self.log('val_true_negative_rate', true_negative_rate, prog_bar=True)
-        self.log('val_false_negative_rate', false_negative_rate, prog_bar=True)
-        self.log('val_true_positive_rate', true_positive_rate, prog_bar=True)
+        self.log('end_classifier/val_loss', val_loss, prog_bar=True)
+        self.log('end_classifier/val_acc', val_acc, prog_bar=True)
+        self.log('end_classifier/val_f1_macro', val_f1_macro, prog_bar=True)
+        self.log('end_classifier/val_false_positive_rate', false_positive_rate, prog_bar=True)
+        self.log('end_classifier/val_true_negative_rate', true_negative_rate, prog_bar=True)
+        self.log('end_classifier/val_false_negative_rate', false_negative_rate, prog_bar=True)
+        self.log('end_classifier/val_true_positive_rate', true_positive_rate, prog_bar=True)
+
+        print(classification_report(targets, preds))
+        self.validation_step_outputs.clear()
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
-        return optimizer
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=1e-8)
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        return [optimizer], [scheduler]
--- training/train_ws.py
+++ training/train_ws.py
@@ -1,17 +1,18 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
-import wandb
 import pandas as pd
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 from model import TransformersClassifier, FakeDataset
 from sklearn.model_selection import StratifiedKFold
 from snorkel.labeling.model import LabelModel
 import argparse
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
-
-pl.seed_everything(42)
+SEED = 42
+pl.seed_everything(SEED, workers=True)
 
 def get_train_test_fold(fold, dataset, model_size, model_name="llama2_platypus", num_splits=10):
     assert fold < num_splits
@@ -33,48 +34,111 @@ def parse_arguments():
     parser.add_argument("--model_size", type=int, choices=[7, 13, 70], required=True)
     parser.add_argument("--model_name", type=str, default="llama2_platypus")
     parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--end_classifier_name", type=str, default="roberta-base")
     
-    
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--num_epochs", type=int, default=20)
+    parser.add_argument("--warmup_steps", type=int, default=100)
+    parser.add_argument("--weight_decay", type=float, default=0.0)
     args = parser.parse_args()
     
     return args
 
 
-def main(fold, dataset, model_size, model_name):
-    df_train, df_test = get_train_test_fold(0, dataset, model_size, model_name)
+def main(fold, dataset, model_size, model_name, pretrained_name, hyperparameters):
+    df_train, df_test = get_train_test_fold(fold, dataset, model_size, model_name)
+    X_train = df_train["text"].tolist()
+    X_test = df_test["text"].tolist()
+    y_test_gold = df_test["objective_true"].to_numpy()
 
-    # Train label model and infer silver labels
+    # Train label model and infer test labels (label model approach)
+    L_ws_train = df_train.iloc[:, :19].to_numpy()
+    L_ws_test = df_test.iloc[:, :19].to_numpy()
+    label_model = LabelModel(cardinality=2, device="cpu", verbose=False)
+    label_model.fit(L_ws_train, n_epochs=500, seed=SEED)
+    y_pred_ws = label_model.predict(L=L_ws_test, tie_break_policy="random")
+    val_acc = accuracy_score(y_test_gold, y_pred_ws)
+    val_f1_macro = f1_score(y_test_gold, y_pred_ws, average='macro')
+
+    tn, fp, fn, tp = confusion_matrix(y_test_gold, y_pred_ws).ravel()
+    false_positive_rate = fp / (fp + tn)
+    true_negative_rate = tn / (tn + fp)
+    false_negative_rate = fn / (fn + tp)
+    true_positive_rate = tp / (tp + fn)
+
+    # Train label model and infer silver labels to fine-tune a classifier (label model + end classifier approach)
     L_ws = df_train.iloc[:, :19].to_numpy()
     label_model = LabelModel(cardinality=2, device="cpu", verbose=False)
-    label_model.fit(L_ws, n_epochs=500, seed=0)
+    label_model.fit(L_ws, n_epochs=500, seed=SEED)
     y_pred_ws = label_model.predict(L=L_ws, tie_break_policy="random")
 
-    # Train with silver labels, evaluate with gold labels
-    X_train = df_train["text"].tolist()
-    y_train = y_pred_ws
+    trainset = FakeDataset(X_train, y_pred_ws, tokenizer_name=pretrained_name)
+    testset = FakeDataset(X_test, y_test_gold, tokenizer_name=pretrained_name)
 
-    X_test = df_test["text"].tolist()
-    y_test = df_test["objective_true"].to_numpy()
+    train_loader = DataLoader(trainset, batch_size=hyperparameters["batch_size"], shuffle=True)
+    val_loader = DataLoader(testset, batch_size=hyperparameters["batch_size"])
 
-    trainset = FakeDataset(X_train, y_train)
-    testset = FakeDataset(X_test, y_test)
+    model = TransformersClassifier(
+        pretrained_name=pretrained_name,
+        num_classes=1,
+        learning_rate=hyperparameters["learning_rate"],
+        warmup_steps=hyperparameters["warmup_steps"],
+        weight_decay=hyperparameters["weight_decay"]
+    )
 
-    train_loader = DataLoader(trainset, batch_size=32, shuffle=True)
-    val_loader = DataLoader(testset, batch_size=32)
+    experiment_config = {
+        "dataset": dataset,
+        "fold": fold,
+        "model_size": model_size,
+        "model_name": model_name
+    }
 
-    model = TransformersClassifier(num_classes=1)
-    # wandb.init(project='prompted_credibility', config=model.hparams)
-    trainer = pl.Trainer()
+    logger = WandbLogger(
+        project="prompted_credibility",
+        name=f"fold-{fold}",
+        log_model=False
+    )
+    logger.experiment.config.update(
+        experiment_config
+    )
+
+    logger.log_metrics({ # log label model approach
+        "label_model/val_acc": val_acc,
+        "label_model/f1_macro": val_f1_macro,
+        "label_model/val_true_positive_rate": true_positive_rate,
+        "label_model/val_false_positive_rate": false_positive_rate,
+        "label_model/val_true_negative_rate": true_negative_rate,
+        "label_model/val_false_negative_rate": false_negative_rate
+    })
+
+    trainer = pl.Trainer(deterministic=True, max_epochs=hyperparameters["num_epochs"], logger=logger)
     trainer.fit(model, train_loader, val_loader)
 
 if __name__ == "__main__":
     args = parse_arguments()
     DATASET = args.dataset
-    FOLD = args.dataset
+    FOLD = args.fold
     MODEL_SIZE = args.model_size
     MODEL_NAME = args.model_name
     DEVICE_NUM = args.device_num
-    
-    main(fold=FOLD, dataset=DATASET, model_name=MODEL_NAME, model_size=MODEL_SIZE)
+    PRETRAINED_NAME = args.end_classifier_name
+
+    hyperparameters = {
+        "learning_rate": args.learning_rate,
+        "batch_size": args.batch_size,
+        "num_epochs": args.num_epochs,
+        "warmup_steps": args.warmup_steps,
+        "weight_decay": args.weight_decay
+    }
+
+    main(
+        fold=FOLD,
+        dataset=DATASET,
+        model_name=MODEL_NAME,
+        model_size=MODEL_SIZE,
+        pretrained_name=PRETRAINED_NAME,
+        hyperparameters=hyperparameters
+    )
 
 
