f7ffbbe1a21c0142cfd6abb0fabcfe9cf6233cc0
Joao Leite <joaoaugustobr@hotmail.com>
2023-09-19 19:01:00 +0100

    attempting to add ft scenario to crossdataset

--- training/cross_dataset.py
+++ training/cross_dataset.py
@@ -1,15 +1,58 @@
 import pandas as pd
 import pytorch_lightning as pl
-from pytorch_lightning.utilities import rank_zero_only
 from sklearn.model_selection import StratifiedKFold
 from snorkel.labeling.model import LabelModel
 import argparse
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_score, recall_score
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import set_peft_model_state_dict
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig
 import wandb
+import torch
+import os
+import random
+from tqdm import tqdm
 
 SEED = 42
 pl.seed_everything(SEED, workers=True)
 
+class llama2_platypus():
+    def __init__(self, size, model_name=None):
+        if model_name is None:
+            if size in [7, 13, 70]:
+                model_name = f"garage-bAInd/Platypus2-{size}B"
+            else:
+                raise Exception(f"Size {size} not available for Llama. Choose 7, 13 or 70.")
+        
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type='nf4',
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, add_eos_token=False, add_bos_token=True)
+        self.model  = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            quantization_config=bnb_config,
+            # load_in_8bit=True,
+            device_map="auto"
+        )
+        self.model = self.model.eval()
+        self.device = self.model.device
+
+    def prompt(self, input, question, system_context):
+        # The number of tokens for the question and prompt formatting amounts to 33 tokens
+        # so we can use 4064 tokens for the input text. Will use 4050 to leave some room.
+        truncate_to = 4050
+        input = self.tokenizer.decode(self.tokenizer.encode(input, return_tensors='pt', truncation=True, max_length=truncate_to, add_special_tokens=False)[0]) # ensure the text will fit 4096 tokens
+        prompt = f"### Instruction:\n{system_context}\n\n### Input:\n{input.strip()}\n\n{question.strip()}\n\n### Response:\n"
+        # ans1 = self.get_next_word_probs(prompt, allow_abstain)
+        ans = self.model.generate(self.tokenizer.encode(prompt, return_tensors='pt').to(self.device), max_new_tokens=1, num_beams=1, do_sample=False)
+        ans = self.tokenizer.decode(ans[0])
+        ans = ans.split("### Response:")[1].strip()
+        return ans
+    
 def aux_get_train_test_fold(fold, dataset, model_size, model_name="llama2_platypus", num_splits=10):
     assert fold < num_splits
     
@@ -93,16 +136,83 @@ def main(fold, training_method, train_dataset, test_dataset, model_size, model_n
         precision = precision_score(y_test_gold, y_pred_ws)
         recall = recall_score(y_test_gold, y_pred_ws)
 
-        wandb.log({
-                "val/acc": val_acc,
-                "val/f1_macro": val_f1_macro,
-                "val/true_positive_rate": true_positive_rate,
-                "val/false_positive_rate": false_positive_rate,
-                "val/true_negative_rate": true_negative_rate,
-                "val/false_negative_rate": false_negative_rate,
-                "val/precision": precision,
-                "val/recall": recall
-        }, step=0)
+    elif training_method == "ft":
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type='nf4',
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            quantization_config=bnb_config,
+            device_map="auto"
+        )
+
+        model_path = f"finetuning/results-{train_dataset}-{fold}/"
+        latest_step = sorted([folder.split("-")[1] for folder in os.listir(model_path)], reverse=True).pop()
+        best_model_path = os.path.join(model_path, latest_step, "adapter_model.bin")
+        adapters_weights = torch.load(best_model_path)
+
+        print("loading checkpoint:", best_model_path)
+        set_peft_model_state_dict(model, adapters_weights)  # set LoRA weights
+
+        inference_model = llama2_platypus(size=MODEL_SIZE, model=model)
+        system_context = """You are a helpful and unbiased news verification assistant. You will be provided with the title and the full body of text of a news article. Then, you will answer further questions related to the given article. Ensure that your answers are grounded in reality, truthful and reliable."""
+        prompt = """{title}\n{text}"""
+        randomizer = lambda: random.randint(0, 1)
+        def class_mapper(answer):
+            if answer.lower().startswith("no") or answer.lower().startswith("false"):
+                category = 0
+            elif answer.lower().startswith("yes") or answer.lower().startswith("true"):
+                category = 1
+            else:
+                category = -1
+
+            return category
+
+
+        preds = []
+        targets = []
+        for i, article_row in tqdm(enumerate(df_test.sample(frac=1).itertuples()), total=len(df_test)):
+            system_context_zs = system_context.format(abstain_context="")
+            input = prompt.format(title=article_row.title, text=article_row.text)
+            question = "Does this article contain misinformation? (Yes/No)"
+            ans = inference_model.prompt(input=input, question=question, system_context=system_context_zs)
+        
+            label = class_mapper(ans)
+
+            preds.append(label)
+            targets.append(article_row.objective)
+
+        num_invalid = len([v for v in preds if v == -1])
+        preds = [v if v != -1 else randomizer() for v in preds]
+
+        val_acc = accuracy_score(targets, preds)
+        val_f1_macro = f1_score(targets, preds, average='macro', zero_division=0.0)
+
+        tn, fp, fn, tp = confusion_matrix(targets, preds).ravel()
+        false_positive_rate = fp / (fp + tn)
+        true_negative_rate = tn / (tn + fp)
+        false_negative_rate = fn / (fn + tp)
+        true_positive_rate = tp / (tp + fn)
+
+        precision = precision_score(targets, preds, zero_division=0.0)
+        recall = recall_score(targets, preds, zero_division=0.0)
+
+    else:
+        raise Exception("Training method not supported.")
+    
+    wandb.log({
+            "val/acc": val_acc,
+            "val/f1_macro": val_f1_macro,
+            "val/true_positive_rate": true_positive_rate,
+            "val/false_positive_rate": false_positive_rate,
+            "val/true_negative_rate": true_negative_rate,
+            "val/false_negative_rate": false_negative_rate,
+            "val/precision": precision,
+            "val/recall": recall
+    }, step=0)
     
 if __name__ == "__main__":
     args = parse_arguments()
