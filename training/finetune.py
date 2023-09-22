52a243bf73918892ab2529d28f51a5412a396f93
Joao Leite <joaoaugustobr@hotmail.com>
2023-09-22 13:43:13 +0100

    fixing finetune dataset pat

--- training/finetune.py
+++ training/finetune.py
@@ -34,7 +34,7 @@ def parse_arguments():
     
     return args
 
-def get_train_test_fold(fold, dataset, num_splits=10):
+def get_train_test_fold(fold, dataset, model_name="llama2_platypus", num_splits=10):
     assert fold < num_splits
 
     system_context = """You are a helpful and unbiased news verification assistant. You will be provided with the title and the full body of text of a news article. Then, you will answer further questions related to the given article. Ensure that your answers are grounded in reality, truthful and reliable."""
@@ -51,28 +51,6 @@ def get_train_test_fold(fold, dataset, num_splits=10):
 
         if fold == j:
             return train_df, test_df
-
-class llama2_platypus():
-    def __init__(self, size, model):
-        if size in [7, 13, 70]:
-            model_name = f"garage-bAInd/Platypus2-{size}B"
-        else:
-            raise Exception(f"Size {size} not available for Llama. Choose 7, 13 or 70.")
-
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, add_eos_token=False, add_bos_token=True)
-        self.model = model
-
-    def prompt(self, input, question, system_context):
-        # The number of tokens for the question and prompt formatting amounts to 33 tokens
-        # so we can use 4064 tokens for the input text. Will use 4050 to leave some room.
-        truncate_to = 4050
-        input = self.tokenizer.decode(self.tokenizer.encode(input, return_tensors='pt', truncation=True, max_length=truncate_to, add_special_tokens=False)[0]) # ensure the text will fit 4096 tokens
-        prompt = f"### Instruction:\n{system_context}\n\n### Input:\n{input.strip()}\n\n{question.strip()}\n\n### Response:\n"
-        # ans1 = self.get_next_word_probs(prompt, allow_abstain)
-        ans = self.model.generate(self.tokenizer.encode(prompt, return_tensors='pt').to("cuda"), max_new_tokens=1, num_beams=1, do_sample=False)
-        ans = self.tokenizer.decode(ans[0])
-        ans = ans.split("### Response:")[1].strip()
-        return ans
     
 class SavePeftModelCallback(TrainerCallback):
     def on_save(
